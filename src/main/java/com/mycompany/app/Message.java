d0cf231518ab58cd05e09815ea2c290c381b702a
BEAUME <sebastien.beaume@capgemini.com>
2018-07-02 15:46:32 +0200

    First commit

---
+++ src/main/java/com/mycompany/app/Message.java
@@ -0,0 +1,17 @@
+package com.mycompany.app;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean(name = "message", eager = true)
+@RequestScoped
+public class Message {
+    private String message = "Hello World!";
+
+    public String getMessage() {
+        return message;
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
